d492d34d5471bca63f46c32f86d18e236873dfb6
Madhura Parikh <madhuraparikh@gmail.com>
2014-09-21 23:15:14 -0500

    create first than hash

--- lab1.cpp
+++ lab1.cpp
@@ -1,12 +1,14 @@
-# include<sys/time.h>
+# include <sys/time.h>
+# include <signal.h>
 # include <errno.h>
-# include<sys/wait.h>
+# include <sys/wait.h>
 # include <stdio.h>
-# include<unistd.h>
-# include<fcntl.h>
-# include<iostream>
+# include <unistd.h>
+# include <fcntl.h>
+# include <iostream>
 # include "src/city.h"
-# include<deque>
+# include <deque>
+
 int  computeHash (void * arg); 
 
 extern "C" 
@@ -23,7 +25,10 @@ struct HashArgs
 
 int computeHash (void * arg) 
 {
-	std::cout << "inside cityhash" << std::endl;
+	pause();
+        char tmp[6] = "city\n";
+	//std::cout << "inside cityhash" << std::endl;
+	//write(STDOUT_FILENO, tmp, 5);
 	HashArgs* hashArgs = (HashArgs *) arg; 
         for (uint64_t  i = 0; i < hashArgs->numHashes; ++i) {
             CityHash128(hashArgs->s, hashArgs->len);
@@ -31,8 +36,14 @@ int computeHash (void * arg)
        return 0;
 }
 
+void sigHandler(int sigNo)
+{
+    return; 
+}
+
 int main(int argc, char*argv[])
 {
+	signal(SIGCONT, sigHandler);
 	int fd = open("/dev/urandom", O_RDONLY);
 	if (fd < 0) {
 	    perror("error reading /dev/urandom");
@@ -69,15 +80,24 @@ int main(int argc, char*argv[])
         for (uint64_t i = 0; i < numThreads; ++i) {
   	    int pid; 
             pid = clone(computeHash, (char *)stacks[i] + stackSize, CLONE_VM | SIGCHLD, arg);
-            if (pid == -1) {
+	    if (pid == -1) {
 		perror("clone error");
 		exit(3);
 	    }
 	    std::cout << "created process " << pid << std::endl;
 	    pids.push_back(pid);
         }
+	//sleep(1); // wait for all LWPs to set up before sending SIG
+        gettimeofday(&tv1, NULL);
+	for (uint64_t i = 0; i < numThreads; ++i) {
+	    if (kill(pids[i], SIGCONT) == -1) {
+		printf("failed to start process %d\n", pids[i]);
+	    }
+	}
         for (uint64_t i = 0; i < numThreads; ++i) {
-           std::cout << "waiting for process " << pids[i] << std::endl;
+          // std::cout << "waiting for process " << pids[i] << std::endl;
+	   char tmp[6] = "wait\n";
+	  // write(STDOUT_FILENO, tmp, 5);
 	   int pid;
 	   pid = waitpid(pids[i], 0, 0);
 	   if (pid == -1) {
@@ -85,13 +105,12 @@ int main(int argc, char*argv[])
 		exit(4);
 	   }
   	}
-        std::cout << "stopping background ..." << std::endl;
+        gettimeofday(&tv2, NULL);
+        //std::cout << "stopping background ..." << std::endl;
         stop();
 	for (uint64_t i = 0; i < numThreads; ++i) {
 	    free(stacks[i]);
 	}
-        gettimeofday(&tv1, NULL);
-        gettimeofday(&tv2, NULL);
         uint64_t tv = (tv2.tv_sec - tv1.tv_sec) * 1000 * 1000 + tv2.tv_usec -tv1.tv_usec;
         printf("Total hash time = %ld.%06ld s\n", tv / 1000000, tv % 1000000);
         return 0;
