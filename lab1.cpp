7258556c6733dc6b6ed7c08e410f01779f225469
Madhura Parikh <madhuraparikh@gmail.com>
2014-09-23 03:20:47 -0500

    use clock_gettime

--- lab1.cpp
+++ lab1.cpp
@@ -1,8 +1,10 @@
 # include<sys/time.h>
 # include <stdio.h>
-# include<unistd.h>
-# include<fcntl.h>
-# include<iostream>
+# include <string.h>
+# include <time.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <iostream>
 # include "src/city.h"
 int main(int argc, char*argv[])
 {
@@ -10,15 +12,15 @@ int main(int argc, char*argv[])
 	char data[4096];
 	read(fd, &data, 4096);
 	close(fd);
-        struct timeval tv1, tv2;
+        struct timespec tv1, tv2;
         uint64_t numHashes;
         std::cin >> numHashes;
-        gettimeofday(&tv1, NULL);
+        clock_gettime(CLOCK_MONOTONIC, &tv1);
         for (int i = 0; i < numHashes; ++i) {
             CityHash128(data, 4096);
         }
-        gettimeofday(&tv2, NULL);
-        uint64_t tv = (tv2.tv_sec - tv1.tv_sec) * 1000 * 1000 + tv2.tv_usec -tv1.tv_usec;
-        printf("Total hash time = %ld.%06ld s\n", tv / 1000000, tv % 1000000);
+        clock_gettime(CLOCK_MONOTONIC, &tv2);
+        uint64_t tv = (tv2.tv_sec - tv1.tv_sec) * 1000000000+ tv2.tv_nsec -tv1.tv_nsec;
+        printf("Total hash time = %ld.%06ld s\n", tv / 1000000000, tv % 1000000000);
         return 0;
 } 
