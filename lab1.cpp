9fc9eccb0f4b24dcdf44e0fed6d037decc7ea34e
Madhura Parikh <madhuraparikh@gmail.com>
2014-09-23 02:45:20 -0500

    use clock_gettime

--- lab1.cpp
+++ lab1.cpp
@@ -1,4 +1,5 @@
 # include <sys/time.h>
+# include <time.h>
 # include <signal.h>
 # include <errno.h>
 # include <sys/wait.h>
@@ -9,6 +10,7 @@
 # include <iostream>
 # include "src/city.h"
 # include <deque>
+# include <string.h>
 # include <string>
 
 int  computeHash (void * arg); 
@@ -20,9 +22,15 @@ int stop();
 }
 struct HashArgs
 {
- const char * s;
+ char s[4096];
  size_t len;
  uint64_t numHashes;
+ HashArgs(const char* m_s, size_t m_len, uint64_t m_numHashes)
+        : len(m_len), numHashes(m_numHashes)
+        {
+          memcpy((void*) s, (void *)m_s, len);
+        }
+
 };
 
 int computeHash (void * arg) 
@@ -64,7 +72,8 @@ int main(int argc, char*argv[])
 	close(fd);
 	std::deque<int> pids;
 	std::deque<void *> stacks;
-        struct timeval tv1, tv2;
+	std::deque<HashArgs> argsList;
+        struct timespec tv1, tv2;
         uint64_t numHashes;
         uint64_t numThreads;
         uint64_t numBackground;
@@ -73,11 +82,6 @@ int main(int argc, char*argv[])
         std::cin >> numHashes;
         std::cin >> numThreads;
         std::cin >> numBackground;
-        HashArgs hashArgs;
-        hashArgs.s = data;
-        hashArgs.len = 4096;
-        hashArgs.numHashes = numHashes;
-        void* arg = (void*)&hashArgs;
         start (numBackground);
 	for (uint64_t i = 0; i < numThreads; ++i) {
 	    void * child_stack = malloc(stackSize);
@@ -89,7 +93,8 @@ int main(int argc, char*argv[])
 	}
         for (uint64_t i = 0; i < numThreads; ++i) {
   	    int pid; 
-            pid = clone(computeHash, (char *)stacks[i] + stackSize, CLONE_VM | SIGCHLD, arg);
+	    argsList.emplace_back(data, 4096, numHashes);
+            pid = clone(computeHash, (char *)stacks[i] + stackSize, CLONE_VM | SIGCHLD, (void*)&argsList[i]);
 	    if (pid == -1) {
 		perror("clone error");
 		exit(3);
@@ -98,7 +103,7 @@ int main(int argc, char*argv[])
 	    pids.push_back(pid);
         }
 	sleep(1); // wait for all LWPs to set up before sending SIG
-        gettimeofday(&tv1, NULL);
+        clock_gettime(CLOCK_MONOTONIC, &tv1);
 	for (uint64_t i = 0; i < numThreads; ++i) {
 	    cgroup_attach_task_pid(hipri, pids[i]);
 	    if (kill(pids[i], SIGCONT) == -1) {
@@ -116,13 +121,13 @@ int main(int argc, char*argv[])
 		exit(4);
 	   }
   	}
-        gettimeofday(&tv2, NULL);
+        clock_gettime(CLOCK_MONOTONIC, &tv2);
         //std::cout << "stopping background ..." << std::endl;
         stop();
 	for (uint64_t i = 0; i < numThreads; ++i) {
 	    free(stacks[i]);
 	}
-        uint64_t tv = (tv2.tv_sec - tv1.tv_sec) * 1000 * 1000 + tv2.tv_usec -tv1.tv_usec;
-        printf("Total hash time = %ld.%06ld s\n", tv / 1000000, tv % 1000000);
+        uint64_t tv = (tv2.tv_sec - tv1.tv_sec) * 1000000000 + tv2.tv_nsec -tv1.tv_nsec;
+        printf("Total hash time = %ld.%06ld s\n", tv / 1000000000, tv % 1000000000);
         return 0;
 } 
